cc4d12d0ac5a5f19a9840babfec87d7ca074966a
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-08-12 14:47:26 -0500

    extractor

--- Tool.py
+++ Tool.py
@@ -72,13 +72,14 @@ def button_click():
     # vedios = os.listdir(folder_path)  #### In place of 'Videos' replace it by the folder name where all the video data is placed
     # print(vedios)
     for ind,vedio in enumerate(os.listdir(folder_path)):
+        print(vedio)
         try:
             # fix video taking input problems
-            path = folder_path+'/'+'New folder'+'/'+vedio  #### In place of 'Videos' replace it by the folder name where all the video data is placed
+            path = folder_path +'/'+ vedio  #### In place of 'Videos' replace it by the folder name where all the video data is placed
             fe = FrameExtractor(path)
             fe.get_video_duration()
             fe.get_n_images(every_x_frame=50) ### Adjust the frame rate as per the length of video
-            fe.extract_frames(every_x_frame=50, img_name=str(ind)+'Name_',dest_path=folder_path+'/'+'vedio')
+            fe.extract_frames(every_x_frame=50, img_name=str(ind)+'Name_',dest_path = folder_path+'/'+ str(ind))
         except Exception as e:
             print(e)
 
